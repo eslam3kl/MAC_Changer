1e39105410c42199e83f86eecd183c98fdb1ce5c
root <root@kali.Kali>
2020-08-18 15:45:08 -0400

    Description par ex : first commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# MAC_Changer
+# MAC_Changer Tool 
+This tool used to change your mac address of specific interface 
+Coded by Eslam Akl -> eslam3kl
+Thanks for Minney
+---------------------------
+
+The argument will the tool take is 2 arguments 
+1. The new MAC address 
+2. The interface in which you want to change it (eth0, en0, etc.. ) 
+Ex
+python mac_changer.py -i eth0 -m 00:11:22:33:44:55
+
+
+Notes: 
+[+] Check that you have entered the correct interface 
+[+] Check that the new mac address is following mac addresses query 
+
+Requirments: 
+Python 2 or 3 
---
+++ mac_changer.py
@@ -0,0 +1,69 @@
+"""
+MAC Changer tool is used to change the mac address of given interface 
+Coded by Eslam Akl -> @eslam3kl 
+Thanks for Minney 
+"""
+#!/usr/bin/env python 
+import subprocess 
+import optparse
+import re
+
+#start the code  
+print(" ") 
+print("***********************************") 
+print ("## MAC changer ## ")
+print(" ")
+print ('[*] Coded by Eslam Akl @eslam3kl')
+print ("[*] Thanks for Minney ")
+print("***********************************") 
+print(" ")
+
+#function to change the mac address
+def change_mac(interface, new_mac):
+	#This is more secure method to get valid user input 
+	subprocess.call(["ifconfig", interface, "down"])
+	subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+	subprocess.call(["ifconfig", interface, "up"])
+
+#function to get the results of entered args
+def get_arguments(): 
+	parser=optparse.OptionParser()
+	parser.add_option("-i","--interface",dest="interface",help="This interface which you want to change")
+	parser.add_option("-m","--mac",dest="new_mac",help="This new mac which you want it")
+	(options, arguments) = parser.parse_args()
+	if not options.interface: 
+		print("[-] Please enter valid interface and MAC address, use --help for more info") 
+		raise SystemExit  
+	elif not options.new_mac: 
+		print("[-] Please enter valid interface and MAC address, use --help for more info")
+		raise SystemExit 
+ 	return options 
+
+def ifconfig_output(interface): 
+	#get the output of the ifconfig results
+	ifconfig_result = subprocess.check_output(["ifconfig", interface])
+
+	#using regex to find the mac address from ifconfig_results 
+	mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+	#check if the user enter valid interface or not
+	if mac_address_search_result: 
+		return mac_address_search_result.group(0)
+	else: 
+		print("[-] Could not find MAC address in the entered interface") 
+		raise SystemExit 
+
+
+options = get_arguments()
+current_mac = ifconfig_output(options.interface)
+print("[+] Your current MAC address is " + str(current_mac))
+change_mac(options.interface, options.new_mac)
+current_mac = ifconfig_output(options.interface)
+
+if current_mac == options.new_mac: 
+	print("[+] Your MAC address is successfully changed to [" + str(current_mac) + "]")
+else: 
+	print("[-] Your MAC address is not changed")
+
+print(" ")
+#end of the code 
